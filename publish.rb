6ed69719293da21935e3f6453692bcc2631ebbad
Trevor Power <trevor_power@yahoo.com>
2012-02-05 20:28:53 +0000

    chache and content type for s3 assets

--- publish.rb
+++ publish.rb
@@ -8,6 +8,10 @@ def publish(name, content)
   )
   bucket = service.buckets.find('milklabs-status')
   new_status = bucket.objects.build(name)  
+  new_status.content_type = 'application/json'
+  new_status.access_control_allow_origin = '*'
+  new_status.content_type = 'application/json'
+  new_status.cache_control = 'no-cache'
   new_status.content = content
   new_status.save
 end
